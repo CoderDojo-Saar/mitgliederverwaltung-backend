8b2fac893f8dd5b251f3a85d9d41a476937ea7a8
Andreas Sander <andreas.sander@hotmail.de>
2018-10-14 13:14:53 +0200

    Finish connection via GraphQL-Yoga to our Prisma DB

--- src/db.js
+++ src/db.js
@@ -1,7 +1,7 @@
 // This file connects to the remote Prisma DB and gives us ability to query w/ JS
 const { Prisma } = require("prisma-binding");
 const db = new Prisma({
-  typeDefs: "src/generated/schema.graphql",
+  typeDefs: "src/generated/prisma.graphql",
   endpoint: process.env.PRISMA_ENDPOINT,
   secret: process.env.PRISMA_SECRET,
   debug: false
