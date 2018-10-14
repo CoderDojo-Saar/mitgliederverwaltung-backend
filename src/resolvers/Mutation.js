f476c5b30007883d802e2a060bd002a7ed9b1949
Andreas Sander <andreas.sander@hotmail.de>
2018-10-14 10:26:46 +0200

    Got GraphQL working with Prisma

---
+++ src/resolvers/Mutation.js
@@ -0,0 +1,11 @@
+const Mutation = {
+  createDog(parent, args, ctx, info) {
+    // Create a dog
+    global.dogs = global.dogs || [];
+    const newDog = { name: args.name };
+    global.dogs.push(newDog);
+    return newDog;
+  }
+};
+
+module.exports = Mutation;
