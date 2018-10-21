24d7d07e2dea3d31284cafeacc01378dc7bf674c
Andreas Sander <andreas.sander@hotmail.de>
2018-10-21 19:52:46 +0200

    Contacts - Query

--- src/resolvers/Query.js
+++ src/resolvers/Query.js
@@ -1,7 +1,8 @@
 const { forwardTo } = require("prisma-binding");
 const Query = {
   eventSeries: forwardTo("db"),
-  participants: forwardTo("db")
+  participants: forwardTo("db"),
+  contacts: forwardTo("db")
 };
 
 module.exports = Query;
